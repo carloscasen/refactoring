de34e8cb565cdfad6dce294a0c3eba14173c85fc
carloscasen <carloscasen677@gmail.com>
2016-04-11 10:00:43 +0200

    añadir getFrequentRenterPoints() en rental

--- refactoring/Customer.java
+++ refactoring/Customer.java
@@ -24,7 +24,6 @@ public class Customer {
 		return name;
 	}
 
-	
 	public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
@@ -33,12 +32,9 @@ public class Customer {
 		while (rentals.hasMoreElements()) {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
-			thisAmount = each.getCharge();
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-				frequentRenterPoints++; // show figures for this rental
+			frequentRenterPoints += each.getFrequentRenterPoints();
+			
+			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
 			totalAmount += each.getCharge();
 		}
